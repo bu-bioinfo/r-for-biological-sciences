c5b064fcf3ac034b4fe0cf4272079998e67c3e9d
Adam Labadorf <alabadorf@gmail.com>
2022-02-08 22:03:02 -0500

    setup in bioinfo

--- content/0700_bioinfo.R
+++ content/0700_bioinfo.R
@@ -1,6 +1,9 @@
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-BiocManager::install(c("SummarizedExperiment","affydata"), update=FALSE, ask=FALSE)
+BiocManager::install(c("SummarizedExperiment","affydata","limma"), update=FALSE, ask=FALSE)
 library(tidyverse)
 library(patchwork)
+library(limma)
+
+source("setup_example_data.R")
