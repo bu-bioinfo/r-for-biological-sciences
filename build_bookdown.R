1469bd9f0bd248deb348c5f303cdb1366f0d165c
Adam Labadorf <alabadorf@gmail.com>
2022-02-22 00:36:33 -0500

    bookdown split by section

--- build_bookdown.R
+++ build_bookdown.R
@@ -1,5 +1,5 @@
 bookdown::render_book("content",
     # css="style.css" is required for <link> tag in HTML index
     # so that custom block styling works
-    output_format=bookdown::gitbook(css="style.css")
+    output_format=bookdown::gitbook(css="style.css",split_by="section")
 )
