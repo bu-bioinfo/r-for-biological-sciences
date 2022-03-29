adc8d5090e836a98be05f1373661755d1bcfc0ca
Adam Labadorf <alabadorf@gmail.com>
2022-03-29 19:49:52 -0400

    switched to bs4_book theme

--- build_bookdown.R
+++ build_bookdown.R
@@ -1,5 +1,6 @@
 bookdown::render_book("content",
     # css="style.css" is required for <link> tag in HTML index
     # so that custom block styling works
-    output_format=bookdown::gitbook(css="style.css",split_by="section")
+    #output_format=bookdown::html_book(theme="darkly",css="style.css",split_by="section")
+    output_format=bookdown::bs4_book(css="style.css",split_by="section")
 )
