d67fd702c02f43efbc7adde087e96d52265498ef
Adam Labadorf <alabadorf@gmail.com>
2023-01-04 18:27:05 -0500

    fixed ensembl gene prefix table issue in bioinfo gene ids

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-all:
+packages:
+	Rscript install_R_packages.R
+	
+all: packages
 	Rscript build_bookdown.R
+	
+	
+	
---
+++ install_R_packages.R
@@ -0,0 +1,11 @@
+# CRAN packages
+#https://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them
+list.of.packages <- c("tidyverse", "ggdendro", "dynamicTreeCut", "fastcluster", "WGCNA", "threejs")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos='http://cran.us.r-project.org')
+
+# Bioconductor packages
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install(c("affy", "affydata", "hgu95av2cdf", "fgsea", "GO.db", "impute"))
