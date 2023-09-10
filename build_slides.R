7dace7d28d517eb18dde98566545d8829f30e8f0
Adam Labadorf <alabadorf@gmail.com>
2023-09-10 12:05:39 -0400

    week 1 and 2 slide refresh

--- build_slides.R
+++ build_slides.R
@@ -9,17 +9,17 @@ output_format <- ioslides_presentation(
 
 #rmd_list <- list.files("content/slides", pattern="*.Rmd", full.names=TRUE)
 rmd_list <-  c(
+  "0300_r_programming.Rmd",
+  "week_2_slides.Rmd",
   "week_1_slides.Rmd"
 )
 c(
-  "week_2_slides.Rmd",
   "week_3_slides.Rmd",
   "week_4_slides.Rmd",
   "week_5_slides.Rmd",
   "0000_introduction.Rmd",
   "0100_data_in_biology.Rmd",
   "0200_preliminaries.Rmd",
-  "0300_r_programming.Rmd",
   "0400_data_wrangling.Rmd",
   "0500_data_sci.Rmd",
   "0600_data_viz.Rmd",
